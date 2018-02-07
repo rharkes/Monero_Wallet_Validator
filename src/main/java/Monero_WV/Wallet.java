e4102772441cdbe8d1fe6f8f9f1be6fd5f235cbd
Rolf_thuis <r.harkes@nki.nl>
2018-02-07 13:55:30 +0100

    Update licence header

--- src/main/java/Monero_WV/Wallet.java
+++ src/main/java/Monero_WV/Wallet.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2018 Rolf
+ * Copyright (C) 2018 R.Harkes & M.van Gorcum
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
